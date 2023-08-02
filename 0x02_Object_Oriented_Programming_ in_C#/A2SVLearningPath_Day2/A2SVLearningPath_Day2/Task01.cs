b6221365491b312b0845e0a5ef1c488d52d22033
abneka <anekahiwot@gmail.com>
2023-08-02 14:26:35 +0300

    'update'

--- 0x02_Object_Oriented_Programming_ in_C#/A2SVLearningPath_Day2/A2SVLearningPath_Day2/Task01.cs
+++ 0x02_Object_Oriented_Programming_ in_C#/A2SVLearningPath_Day2/A2SVLearningPath_Day2/Task01.cs
@@ -4,19 +4,11 @@ namespace A2SVLearningPath_Day2
 {
     public class Task01
     {
-        public static void PrintShapeArea(object obj)
+        public static void PrintShapeArea(Shape obj)
         {
-            if (obj is Shape shape)
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("Shape: " + shape.Name);
-                Console.WriteLine("Area: " + shape.CalculateArea());
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Invalid object Type");
-            }
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Shape: " + obj.Name);
+            Console.WriteLine("Area: " + obj.CalculateArea());
         }
     }
 }
