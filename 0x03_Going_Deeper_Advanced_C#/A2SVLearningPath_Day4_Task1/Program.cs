d1c7802c2b04b704701ab95910b8203dd0c0f71d
abneka <anekahiwot@gmail.com>
2023-08-03 14:26:37 +0300

    'update;

---
+++ 0x03_Going_Deeper_Advanced_C#/A2SVLearningPath_Day4_Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A2SVLearningPath_Day4_Task1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
